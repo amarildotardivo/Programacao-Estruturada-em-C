28a7985c1c072a6dbe19b320ea5b02b9f5f191cc
Amarildo Tardivo <amarildotardivo@gmail.com>
2021-11-11 20:46:25 -0300

    Ordenação

---
+++ P2/Atividade 13 - ORDENAÇÃO/exercicio.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define tam 2
+
+void preenche(int vet);
+void ordena(int vet);
+void imprimi(int vet);
+
+int main(){
+    int vet[tam];
+    
+    preenche(vet);
+    ordena(vet);
+    imprimi(vet);
+
+    return 0;
+}
+
+void preenche(int vet){
+    
+}
+void ordena(int vet){
+
+}
+void imprimi(int vet){
+
+}
