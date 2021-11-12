63dd54c34647f4a7554508373860fceca61d0ae5
Amarildo Tardivo <amarildotardivo@gmail.com>
2021-11-11 23:43:50 -0300

    Update exercicio.c

--- P2/Atividade 13 - ORDENAÇÃO/exercicio.c
+++ P2/Atividade 13 - ORDENAÇÃO/exercicio.c
@@ -1,27 +1,51 @@
 #include <stdio.h>
 
-#define tam 2
+#define tam 4
 
-void preenche(int vet);
-void ordena(int vet);
-void imprimi(int vet);
+void preenche(int *vet);
+void ordena(int *vet);
+void imprimi(int *vet);
 
 int main(){
     int vet[tam];
     
     preenche(vet);
+    printf("\nVetor não ordenado:\n");
+    imprimi(vet);
+
     ordena(vet);
+    printf("\nVetor ordenado:\n");
     imprimi(vet);
 
     return 0;
 }
 
-void preenche(int vet){
-    
-}
-void ordena(int vet){
+void preenche(int *vet){
+    int i;
+
+    for(i = 0; i < tam; i++){
+        printf("\nInsira o numero %d: ", i + 1);
+        scanf("%d", &vet[i]);
+    }
 
 }
-void imprimi(int vet){
+void ordena(int *vet){
+    int i, j, valor;
 
+    for(i = 1; i < tam; i++){
+        valor = vet[i];
+        for(j = i - 1; (j >= 0) && (valor < vet[j]); j-- ){
+            vet[j+1] = vet[j];
+        }
+        vet[j + 1] = valor;
+    }
+
+}
+void imprimi(int *vet){
+    int i;
+
+    for(i = 0; i < tam; i++){
+       printf("%d ", vet[i]);
+    }
+    printf("\n\n");
 }
